0a74c8375ac4e1684cd217780a60e7093deef861
Phillip Vendel Glimø <phillip.glimoe@hotmail.com>
2021-11-25 13:19:31 +0100

    create route activity

---
+++ app/src/main/java/com/codecademy/christiansoe/activity/RouteActivity.java
@@ -0,0 +1,16 @@
+package com.codecademy.christiansoe.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.codecademy.christiansoe.R;
+
+public class RouteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route);
+    }
+}
