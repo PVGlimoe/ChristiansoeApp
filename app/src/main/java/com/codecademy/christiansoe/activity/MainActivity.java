e7d0f68d5b6395f49c63495768f92237cb8b2c88
JooergenKoeldal <joer1894@stud.kea.dk>
2021-12-02 10:57:36 +0100

    remove old gps tracking

--- app/src/main/java/com/codecademy/christiansoe/activity/MainActivity.java
+++ app/src/main/java/com/codecademy/christiansoe/activity/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 
-import com.codecademy.christiansoe.GpsTracker;
 import com.codecademy.christiansoe.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,10 +25,6 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void showGps(View view){
-        Intent intent = new Intent(this, GpsTracker.class);
-        startActivity(intent);
-    }
 
 
 
--- app/src/main/java/com/codecademy/christiansoe/helper/GpsPosition.java
+++
@@ -1,6 +0,0 @@
-package com.codecademy.christiansoe.helper;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class GpsPosition extends AppCompatActivity {
-}
